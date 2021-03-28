cb5665f3157870a08c1831a7ecb890d577600bb4
Edouard Misset <edouardmisset@gmail.com>
2021-03-28 19:22:02 +0200

    First working CRA Contact App

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Contact from './components/Contact';
+
+function App() {
+  return <Contact />;
+}
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import './Contact.css';
+
+const firstName = 'John';
+const lastName = 'Doe';
+const fullName = `${firstName} ${lastName}`;
+const avatarUrl = `https://eu.ui-avatars.com/api/?name=${firstName}+${lastName}&background=random&rounded=true`;
+const online = true;
+
+function Contact() {
+  return (
+    <div className='Contact'>
+      <img src={avatarUrl} alt='avatar' className='avatar' />
+      <div className='text'>
+        <h4 className='name'>{fullName} </h4>
+        <div className='status'>
+          <span className={online ? 'status-online' : 'status-offline'}></span>
+          <p className='status-text'> {online ? 'Online' : 'Offline'} </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
